200d1d55de2296c50579ed78bfacee1d699d6d0e
livantius <76242373+livantius@users.noreply.github.com>
2020-12-23 00:38:59 +0700

    Добавление и настройка меню с переходом на доп. окна

---
+++ app/src/main/java/a/event_handling/Cm_Gs.java
@@ -0,0 +1,14 @@
+package a.event_handling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cm_Gs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cm_gs);
+    }
+}
--- app/src/main/java/a/event_handling/MainActivity.java
+++ app/src/main/java/a/event_handling/MainActivity.java
@@ -1,8 +1,13 @@
 package a.event_handling;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.bt_cl:
+                Intent intent = new Intent(this, Bt_Cl.class);
+                startActivity(intent);
+                break;
+            case R.id.cm_gs:
+                intent = new Intent(this, Cm_Gs.class);
+                startActivity(intent);
+                break;
+            case R.id.mt_ev:
+                intent = new Intent(this, Mt_Ev.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
