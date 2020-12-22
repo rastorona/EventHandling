5402b1a2042a1872121e85800810f409f618e5cf
livantius <76242373+livantius@users.noreply.github.com>
2020-12-23 00:47:03 +0700

    Наполнение и настройка окна «ButtonClick»

--- app/src/main/java/a/event_handling/Bt_Cl.java
+++ app/src/main/java/a/event_handling/Bt_Cl.java
@@ -3,12 +3,34 @@ package a.event_handling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Bt_Cl extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bt_cl);
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                textView.setText("Button clicked");
+            }
+        });
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View v) {
+                textView.setText("Long button click");
+                return true;
+            }
+        });
     }
 }
